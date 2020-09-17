299414c9d5356e1fb22937ab6824f417d72d5ddd
efbar <17676301+efbar@users.noreply.github.com>
2020-09-17 14:20:12 +0200

    remove proxy

--- externals.tf
+++ externals.tf
@@ -1,27 +1,27 @@
-resource "consul_node" "devops-proxy" {
-  address = "172.23.2.11"
-  name    = "devops-proxy"
-  meta = {
-    external-node  = true
-    external-probe = true
-  }
-}
-resource "consul_service" "devops-proxy-http" {
-  name = "${consul_node.devops-proxy.name}-http"
-  node = consul_node.devops-proxy.name
-  port = 8080
-  meta = {
-    external-source = "terraform"
-  }
+# resource "consul_node" "devops-proxy" {
+#   address = "172.23.2.11"
+#   name    = "devops-proxy"
+#   meta = {
+#     external-node  = true
+#     external-probe = true
+#   }
+# }
+# resource "consul_service" "devops-proxy-http" {
+#   name = "${consul_node.devops-proxy.name}-http"
+#   node = consul_node.devops-proxy.name
+#   port = 8080
+#   meta = {
+#     external-source = "terraform"
+#   }
 
-  check {
-    check_id                          = "service:devops-proxy-http"
-    name                              = "HTTP health check"
-    status                            = "passing"
-    tls_skip_verify                   = true
-    http                              = "http://172.23.2.11:8080/noindex/css/bootstrap.min.css"
-    interval                          = "15s"
-    timeout                           = "3s"
-    deregister_critical_service_after = "30s"
-  }
-}
+#   check {
+#     check_id                          = "service:devops-proxy-http"
+#     name                              = "HTTP health check"
+#     status                            = "passing"
+#     tls_skip_verify                   = true
+#     http                              = "http://172.23.2.11:8080/noindex/css/bootstrap.min.css"
+#     interval                          = "15s"
+#     timeout                           = "3s"
+#     deregister_critical_service_after = "30s"
+#   }
+# }
