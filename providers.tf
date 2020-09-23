c91938fd04679e75206cca55fd49ffd19dbee2ee
efbar <17676301+efbar@users.noreply.github.com>
2020-09-23 15:26:12 +0200

    add tls

--- providers.tf
+++ providers.tf
@@ -1,18 +1,32 @@
 provider "vault" {
   address         = var.vault_endpoint
   skip_tls_verify = var.vault_skip_tls_verify
+  ca_cert_file    = var.ca_cert_file
+  client_auth {
+    cert_file       = var.cert_file
+    key_file        = var.key_file
+  }
 }
-provider "consul" {
-  address = var.consul_endpoint
-  token   = data.vault_generic_secret.consul_bootstrap_token.data["secretid"]
-  insecure_https = true
-  scheme = "https"
-}
+
 data "vault_generic_secret" "consul_bootstrap_token" {
   path = "secret/consul/bootstrap_token"
 }
 
+provider "consul" {
+    address         = var.consul_endpoint
+    insecure_https  = false
+    ca_file         = var.ca_cert_file
+    cert_file       = var.cert_file
+    key_file        = var.key_file
+    scheme          = "https"
+    token           = data.vault_generic_secret.consul_bootstrap_token.data["secretid"]
+}
+
+
 provider "nomad" {
-  address = var.nomad_endpoint
-  region  = "global"
+    address         = var.nomad_endpoint
+    region          = "global"
+    ca_file         = var.ca_cert_file
+    cert_file       = var.cert_file
+    key_file        = var.key_file
 }
