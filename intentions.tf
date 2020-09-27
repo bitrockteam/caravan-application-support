479452ae923e43e2fe158bcce29e8a31b87ca835
efbar <17676301+efbar@users.noreply.github.com>
2020-09-27 16:01:44 +0200

    add some inentions

--- intentions.tf
+++ intentions.tf
@@ -1,3 +1,14 @@
+resource "consul_intention" "consul_to_all" {
+  source_name      = "consul"
+  destination_name = "*"
+  action           = "allow"
+}
+resource "consul_intention" "all_to_consul" {
+  source_name      = "*"
+  destination_name = "consul"
+  action           = "allow"
+}
+
 resource "consul_intention" "esm" {
   source_name      = "poc-esm"
   destination_name = "*"
@@ -16,15 +27,21 @@ resource "consul_intention" "logstash-http" {
   action           = "allow"
 }
 
-resource "consul_intention" "ingress_echo_service" {
-  source_name      = "poc-ingress"
-  destination_name = "spring-echo-example"
+resource "consul_intention" "jaeger-agent" {
+  source_name      = "*"
+  destination_name = "jaeger-agent"
   action           = "allow"
 }
 
-resource "consul_intention" "ingress_jaeger_query" {
+resource "consul_intention" "ingress_jaeger-query" {
   source_name      = "poc-ingress"
   destination_name = "jaeger-query"
+  action           = "allow"
+}
+
+resource "consul_intention" "ingress_kibana" {
+  source_name      = "poc-ingress"
+  destination_name = "kibana"
   action           = "allow"
 }
 
@@ -34,20 +51,38 @@ resource "consul_intention" "ingress_grafana" {
   action           = "allow"
 }
 
-resource "consul_intention" "jaeger-query-jaeger-collector" {
+resource "consul_intention" "ingress_elastic" {
+  source_name      = "poc-ingress"
+  destination_name = "elastic-internal"
+  action           = "allow"
+}
+
+resource "consul_intention" "jaeger-query_jaeger-collector" {
   source_name      = "jaeger-query"
   destination_name = "jaeger-collector"
   action           = "allow"
 }
 
-resource "consul_intention" "jaeger-collector-elastic-internal" {
+resource "consul_intention" "jaeger-collector_elastic-internal" {
   source_name      = "jaeger-collector"
   destination_name = "elastic-internal"
   action           = "allow"
 }
 
-resource "consul_intention" "jaeger-query-elastic-internal" {
+resource "consul_intention" "jaeger-query_elastic-internal" {
   source_name      = "jaeger-query"
   destination_name = "elastic-internal"
+  action           = "allow"
+}
+
+resource "consul_intention" "kibana_elastic-internal" {
+  source_name      = "kibana"
+  destination_name = "elastic-internal"
+  action           = "allow"
+}
+
+resource "consul_intention" "opentraced-app_kibana" {
+  source_name      = "opentraced-app"
+  destination_name = "kibana"
   action           = "allow"
 }
