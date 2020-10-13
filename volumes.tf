f1cd1ef83b8957be1180842e4a054b3c66981cd8
efbar <17676301+efbar@users.noreply.github.com>
2020-10-13 18:25:08 +0200

    add some deps

--- volumes.tf
+++ volumes.tf
@@ -1,11 +1,17 @@
 data "nomad_plugin" "gcepd" {
   count            = var.cloud == "gcp" ? 1 : 0
+  depends_on       = [
+    nomad_job.csi
+  ]
   plugin_id        = "gcepd"
   wait_for_healthy = true
 }
 resource "nomad_volume" "jenkins_master" {
   count           = var.cloud == "gcp" ? 1 : 0
-  depends_on      = [data.nomad_plugin.gcepd]
+  depends_on      = [
+    nomad_job.csi,
+    data.nomad_plugin.gcepd
+  ]
   type            = "csi"
   plugin_id       = "gcepd"
   volume_id       = "jenkins-master"
