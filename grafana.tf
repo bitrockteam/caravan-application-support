7c83b1c319809c99daae3d2e7d2d2058bd4ddd32
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-03-15 14:38:29 +0100

    fix: more depends_on and check grafana (#31)

--- grafana.tf
+++ grafana.tf
@@ -4,14 +4,22 @@ locals {
   } : {}
 }
 
+resource "null_resource" "grafana_healthy" {
+  count = var.configure_grafana ? 1 : 0
+  provisioner "local-exec" {
+    command     = "while [ $(curl -k --silent --output /dev/null --write-out '%%{http_code}' https://grafana.${var.domain}/api/health) != \"200\" ]; do echo \"Waiting grafana reachable...\"; sleep 10; done; sleep 30"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
 resource "grafana_dashboard" "dashboard" {
-  depends_on  = [nomad_job.consul-ingress]
+  depends_on  = [grafana_data_source.metrics]
   for_each    = local.dashboards
   config_json = file(each.value)
 }
 
 resource "grafana_data_source" "metrics" {
-  depends_on = [nomad_job.consul-ingress]
+  depends_on = [null_resource.grafana_healthy, consul_config_entry.grafana_internal]
   count      = var.configure_grafana ? 1 : 0
   type       = "prometheus"
   name       = "Prometheus"
